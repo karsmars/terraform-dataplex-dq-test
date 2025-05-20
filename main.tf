aad43a59eb75a80e190b2754f543c7a7bc0c4511
Spencer Halverson <spencerbroham@gmail.com>
2025-05-19 19:07:29 -0500

    Change scan timing

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "google_dataplex_datascan" "full_quality" {
 
         trigger {
             schedule {
-                cron = "TZ=America/Chicago 1 3 * * *"
+                cron = "TZ=America/Chicago 1 4 * * *"
             }
         }
     }
