164a9c16b05f062915ce5a0cc6033421d4cd469b
Spencer Halverson <spencerbroham@gmail.com>
2025-05-26 11:41:33 -0500

    Remove duplicate scans

--- modules/samples/shakespeare.tf
+++ modules/samples/shakespeare.tf
@@ -30,37 +30,3 @@ labels           = {}
     }
   }
 }
-
-resource "google_dataplex_datascan" "basic_quality" {
-  data_scan_id     = "tf-dataquality-basic"
-labels           = {}
-  location         = "us-central1"
-  project          = "ferrous-cipher-432403-j0"
-  
-  data {
-    resource = "//bigquery.googleapis.com/projects/bigquery-public-data/datasets/samples/tables/shakespeare"
-  }
-  
-  data_quality_spec {
-    sampling_percent = 100
-    
-    rules {
-      description = "rule 1 for validity dimension"
-      dimension   = "VALIDITY"
-      ignore_null = false
-      name        = "rule1"
-      threshold   = 0
-      
-      table_condition_expectation {
-        sql_expression = "COUNT(*) > 0"
-      }
-    }
-  }
-  
-  execution_spec {
-    trigger {
-    on_demand {}
-    }
-  }
-}
-
